300bc8b37db4aec8e58029042238175215bc2519
Carl Bennett <carl@carlbennett.me>
2018-05-29 03:11:04 -0500

    Form the directory structure (#1)
    * Form the directory structure
    
    * Fix casing on BNETDocs brand
    
    * Use python3 instead of python2

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='community-bot',
+      version='0.1',
+      description='BNETDocs Community Bot',
+      author='BNETDocs Community',
+      author_email='staff@bnetdocs.org',
+      license='MIT',
+      url='https://bnetdocs.org/',
+     )
